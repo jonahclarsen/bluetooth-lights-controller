7e5bdd3795e878d1fc976ff48024f81b91fdfc13
Jonah <97376233+jonahclarsen@users.noreply.github.com>
2023-10-01 13:24:26 -0300

    Fix name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='bluetooth_lights_controller',
+    name='bluetooth-lights-controller',
     version='1.0',
 
     description='Govee Bluetooth RGB LED Controller',
@@ -9,7 +9,7 @@ setup(
     author='Jonah Larsen',
     author_email='email@email.com',
 
-    packages=['bluetooth_lights_controller'],
+    packages=['bluetooth-lights-controller'],
     install_requires=[
         'bleak',
         'colour'
