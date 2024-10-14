6b450d21f513203e2c4c4ac4e523501618be010c
Jonah <97376233+jonahclarsen@users.noreply.github.com>
2024-10-14 14:58:39 -0300

    Add some unfinished opts

--- bluetooth_lights_controller/lights_controller.py
+++ bluetooth_lights_controller/lights_controller.py
@@ -5,7 +5,9 @@ timeout = 5.0  # Seems to work well
 
 async def set_color_of_light(mac_address, color, brightness, is_h6005 = False):
     led = BluetoothLED(mac_address, timeout=timeout)
+
     boolt = await led.init_and_connect()
+
     if boolt is False:
         return
     # await led.set_state(True)  # If your lights are ever turned off (i.e. via the app), uncomment this
@@ -29,8 +31,19 @@ async def set_color_of_light_white(mac_address, color, brightness, is_h6005 = Fa
     # using 0.01 until MacOS 14 when it no longer worked, I had to switch to .05 like in the other func)
     await led.disconnect()
 
-
+from pprint import pprint
 async def set_color_of_all_lights(lights, color, brightness):
+    # Optimization: first, get the services of one of the LEDs:
+    # led1 = BluetoothLED(lights["bedroom-1"], None, timeout=timeout)
+    # boolt = await led1.init_and_connect()
+    # services = led1._bt.services
+    # To find out what services this devices uses so we can filter by them when connectingZ:
+    # # pprint(services._BleakGATTServiceCollection__services)
+    # # pprint(services._BleakGATTServiceCollection__services[12].uuid)
+    # # pprint(services._BleakGATTServiceCollection__services[15].uuid)
+    # # pprint(services._BleakGATTServiceCollection__services[23].uuid)
+    # await led1.disconnect()
+
     i = 0
     tasks = []
     for light_name, mac_address in lights.items():
