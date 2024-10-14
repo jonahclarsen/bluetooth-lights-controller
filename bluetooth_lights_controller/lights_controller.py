7884dbfc7dd4b6e4c002b135677c9490a94cb0f7
Jonah <97376233+jonahclarsen@users.noreply.github.com>
2024-10-14 14:30:39 -0300

    Create set_state_of_light()

--- bluetooth_lights_controller/lights_controller.py
+++ bluetooth_lights_controller/lights_controller.py
@@ -4,6 +4,14 @@ from .bluetooth_led import BluetoothLED
 
 timeout = 5.0  # Seems to work well
 
+async def set_state_of_light(mac_address, state, is_h6005 = False):
+    led = BluetoothLED(mac_address, timeout=timeout)
+    boolt = await led.init_and_connect()
+    if boolt is False:
+        return
+    await led.set_state(state)
+    await led.disconnect()
+
 async def set_color_of_light(mac_address, color, brightness, is_h6005 = False):
     led = BluetoothLED(mac_address, timeout=timeout)
     boolt = await led.init_and_connect()
@@ -41,6 +49,24 @@ async def set_color_of_all_lights(lights, color, brightness):
                 is_h6005 = True
 
             tasks += [asyncio.create_task(set_color_of_light(mac_address, color, brightness, is_h6005))]
+            i += 1
+        except Exception as e:
+            print("Exception in lights_controller:", e)
+
+    for task in tasks:
+        await task
+
+
+async def set_state_of_all_lights(lights, state):
+    i = 0
+    tasks = []
+    for light_name, mac_address in lights.items():
+        try:
+            is_h6005 = False
+            if "h6005" in light_name or "H6005" in light_name:
+                is_h6005 = True
+
+            tasks += [asyncio.create_task(set_state_of_light(mac_address, state, is_h6005))]
             i += 1
         except Exception as e:
             print("Exception in lights_controller:", e)
